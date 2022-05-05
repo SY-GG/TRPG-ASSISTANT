ba7678ede64e641be6bb5c4180b6b85bcd91357f
Se0 <sy.park.HUB@gmail.com>
2022-05-05 17:46:43 +0900

    임의 추가 버튼, div 추가

--- js/split.js
+++ js/split.js
@@ -1,18 +1,52 @@
 const scriptForm = document.querySelector('#scriptForm');
 const scriptToChange = document.querySelector('textarea[name="scriptToChange"]');
+
 scriptForm.addEventListener('submit', submitScript);
 
-// 확인 버튼 누르면
 function submitScript(event) {
   event.preventDefault();
   let result = trimScript(scriptToChange.value);
-  console.log(result)
+  console.log(result); //이후 result를 각각 div에 더하기
 }
 
-//TEXT 다듬고 자르기:
 function trimScript(scriptToChange) {
   let result = scriptToChange.replaceAll(/[\n]{1,}/g, '');
   result = result.replaceAll(/([.]{2,})/g, '…');
   result = result.split(/(?<=[!?.])/);
   return result;
+}
+
+//임의 추가 버튼
+const addOptionBtn = document.querySelector('button[name="addOptionBtn"]');
+addOptionBtn.addEventListener('click', () => {
+  makeResultDiv('');
+});
+console.log(addOptionBtn);
+
+function makeResultDiv(textValue) {
+  const resultDiv = document.querySelector('.fieldset-result div');
+  const newResultInput = makeArticle(textValue);
+  resultDiv.appendChild(newResultInput);
+}
+
+// article 태그
+//   ㄴinput (textValue)
+//   ㄴbutton (복사 func.)
+function makeArticle(textValue) {
+  const newArticle = document.createElement('article');
+
+  const newInput = document.createElement('input');
+  newInput.type = 'text';
+  newInput.className = 'splited-script';
+  newInput.value = textValue;
+
+  const newPasteBtn = document.createElement('button');
+  newPasteBtn.type = 'button';
+  newPasteBtn.className = 'copyBtn';
+  newPasteBtn.textContent = '복사';
+
+  newArticle.appendChild(newInput);
+  newArticle.appendChild(newPasteBtn);
+
+  return newArticle;
 }
