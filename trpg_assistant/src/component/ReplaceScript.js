5043053004ca0eadb04a2fff8b2e8beda0a605c0
Se0 <sy.park.HUB@gmail.com>
2022-06-19 16:14:30 +0900

    replaceScript 구분자, 바꿀 스크립트 useState를 통해 관리

--- trpg_assistant/src/component/ReplaceScript.js
+++ trpg_assistant/src/component/ReplaceScript.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/index.css';
 import '../css/cssReset.css';
 
 const TrimSentence = () => {
+  // 스크립트 수정
+  const [script, setScript] = useState('');
+
+  const onChangeScript = (e) => {
+    setScript(e.target.value);
+  };
+
+  const submitScript = (e) => {
+    e.preventDefault();
+  };
+
+  // 구분자
+  const [delimeter, setDelimeter] = useState('.');
+
+  const onChangeDelimeter = (e) => {
+    setDelimeter(e.target.value);
+  };
+
   return (
-    <>
+    <form onSubmit={submitScript}>
       <fieldset id="fieldset-replace">
         <legend>공백 제거/변환</legend>
         <label>
-          구분자: <input name="delimeter" value="." placeholder="구분자를 입력해주세요" />
+          구분자:{' '}
+          <input name="delimeter" value={delimeter} placeholder="구분자를 입력해주세요" onChange={onChangeDelimeter} />
         </label>
         <details>
           <summary>단어 바꾸기</summary>
@@ -20,17 +39,27 @@ const TrimSentence = () => {
           </button>
         </details>
         <section>
-          <textarea class="scriptToChange" name="scriptToChange" placeholder="바꿀 내용을 이곳에 입력해주세요." />
+          <textarea
+            className="scriptToChange"
+            name="scriptToChange"
+            placeholder="바꿀 내용을 이곳에 입력해주세요."
+            onChange={onChangeScript}
+            value={script}
+          />
           <div>
-            <button type="submit">❯</button>
-            <button type="button" name="scriptConvertBtn">
-              ❮
-            </button>
+            <input type="submit" value="❯"></input>
+            {/* <input type="button" name="scriptConvertBtn" value="❮"></input> */}
           </div>
-          <textarea class="scriptResult" name="scriptResult" placeholder="공백 제거 결과는 여기에 보입니다." />
+          {/* <textarea
+            className="scriptResult"
+            name="scriptResult"
+            placeholder="공백 제거 결과는 여기에 보입니다."
+            value={script}
+            ref={resultDiv}
+          /> */}
         </section>
       </fieldset>
-    </>
+    </form>
   );
 };
 
